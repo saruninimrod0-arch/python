8b43bb3baa7d8b0ee0fa892b62737547d637074c
saruninimrod0-arch <saruninimrod0@gmail.com>
2026-02-05 11:48:30 +0300

    add code

---
+++ assignment1.py
@@ -0,0 +1,22 @@
+#Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (weight) / (height squared)
+# Define variables
+weight = 78  # in kg
+height = 1.75  # in meters
+
+# Calculate BMI
+BMI = weight / (height ** 2)
+
+# Print result
+print("Your BMI is:", round(BMI, 2))
+
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+Length = 48 
+Width = 25 
+
+Area = (Length * Width) = 48 * 25 = 1200 
+Perimeter = 2 * (Length + Width) = 2 * (48 + 25) = 2 * 73 = 146
+print("this is the area")
+print("this is perimeter")
+
+# Research on python list, tuple and Dictionary Data types.
